0e67db9bdd471578272e5c440481cd921e6bd265
RobEriks <Rob.Eriksson@outlook.com>
2024-11-19 20:57:29 +0100

    First

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -1,8 +1,28 @@
 // Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
 
-// Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
+fetch('https://api.sr.se/api/v2/channels/?format=json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
+    data.channels.forEach((channel) => {
+      console.log(channel);
+      
+     // Steg 3. ta ut liveaudio.url från varje kanal och lägg i en audio tagg.
+      const audio = document.createElement('audio');
+      audio.controls = true;
+      const source = document.createElement('source');
+      source.src = channel.liveaudio.url;
+      source.type = 'audio/mpeg';
+      audio.appendChild(source);
+      document.body.appendChild(audio);
+    });
+    
+  });
 
-// Steg 3. ta ut liveaudio.url från varje kanal och lägg i en audio tagg.
-// <audio controls>
-//   <source src="" type="audio/mpeg" />
-// </audio>
+  
+
+  
+
+   
